b2838b2528823a79d60b8cea44b4931e6b493cef
Administrator <13923813589@163.com>
2018-02-04 10:09:06 +0800

    Initial commit

---
+++ src/service/IgetCKXX.java
@@ -0,0 +1,8 @@
+package service;
+
+/**
+ * Created by Administrator on 2018/2/3 0003.
+ */
+public interface IgetCKXX {
+    public String getCKXX(String idnum);
+}
---
+++ src/service/getCKXXImpl.java
@@ -0,0 +1,11 @@
+package service;
+
+/**
+ * Created by Administrator on 2018/2/3 0003.
+ */
+public class getCKXXImpl implements IgetCKXX {
+    public String getCKXX(String idnum){
+
+        return "getCKXXImpl-----"+idnum;
+    }
+}
